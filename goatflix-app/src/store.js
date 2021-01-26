59be7a5c0b290c61d3a992158938db22c630b854
GDusek <70817559+GDusek@users.noreply.github.com>
2021-01-26 14:47:17 -0600

    Redux store created

---
+++ goatflix-app/src/store.js
@@ -0,0 +1,13 @@
+
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const initialState = {};
+
+// http://extension.remotedev.io/#12-advanced-store-setup
+// Needed for use with middleware (redux-thunk)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(thunk)));
+
+export default store;
