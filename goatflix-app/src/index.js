38ae749e142df37fe16976ea8c0f9b9a4c024b36
GDusek <70817559+GDusek@users.noreply.github.com>
2021-01-29 03:27:29 -0600

    index.js updated for serviceWorker.js file

--- goatflix-app/src/index.js
+++ goatflix-app/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 import App from './components/App';
+import * as serviceWorker from './serviceWorker';
 
 require('dotenv').config();
 
@@ -15,4 +16,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
+serviceWorker.unregister();
