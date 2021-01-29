a5cd5db0def57c9e88321e3323977a2935d37fa8
GDusek <70817559+GDusek@users.noreply.github.com>
2021-01-28 22:09:53 -0600

    Added dotenv to index.js

--- goatflix-app/src/index.js
+++ goatflix-app/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
-import './index.css';
 import App from './components/App';
 
-
+require('dotenv').config();
 
 ReactDOM.render(
   <Router>
