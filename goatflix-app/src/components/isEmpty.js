07862c31b9fded8904b1ab1cbea7bce06a34b32a
GDusek <70817559+GDusek@users.noreply.github.com>
2021-01-28 21:59:16 -0600

    updated description in the file

--- goatflix-app/src/components/isEmpty.js
+++
@@ -1,9 +0,0 @@
-//This will return when an object, array, or string is empty.
-
-const isEmpty = obj =>
-        obj === undefined ||
-        obj === null ||
-        (typeof obj === 'object' && Object.keys(obj).length === 0) ||
-        (typeof obj === 'string' && obj.trim().length === 0);
-
-module.exports = isEmpty;
