fcdcad78abc3712474936a7b3fd3f3479693bb1c
GDusek <70817559+GDusek@users.noreply.github.com>
2021-01-31 15:41:49 -0600

    Tweaked button styling

---
+++ goatflix-app/src/components/Form/Button.styled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+	padding: 1rem 2rem;
+	border-radius: 10px;
+	border: solid 3px #ffb500;
+	font-size: 1.2rem;
+	background: #111;
+	color: #ffb500;
+	margin: 0 2rem;
+	:hover {
+		cursor: pointer;
+		background: rgb(30,30,30);
+	}
+`;
+
+export default StyledButton;
