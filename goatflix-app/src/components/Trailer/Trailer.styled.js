083aba2819d0ba3857629d8a13068d25f45958da
GDusek <70817559+GDusek@users.noreply.github.com>
2021-01-31 16:38:45 -0600

    Created trailer component & styling

---
+++ goatflix-app/src/components/Trailer/Trailer.styled.js
@@ -0,0 +1,15 @@
+// TODO: Make this actually keep an aspect ratio when resizing: https://css-tricks.com/aspect-ratio-boxes/
+import styled from 'styled-components';
+
+export const TrailerContainer = styled.div`
+	height: 500px;
+`;
+
+export const Title = styled.h2`
+	margin: 2.5rem 0 2rem;
+`;
+
+export const YoutubeFrame = styled.iframe`
+	width: 100%;
+	min-height: 420px;
+`;
