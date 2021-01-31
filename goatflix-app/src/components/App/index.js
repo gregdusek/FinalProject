36ea7c215841e24d1af51879ae3d02ce5fae01dc
GDusek <70817559+GDusek@users.noreply.github.com>
2021-01-31 02:40:58 -0600

    Frontend App index.js file created.
    Dependencies, store, auth, login, and token added
    
    Tried to add TV routes & dependencies in,
    
    but will need to refactor code to make TV work.

---
+++ goatflix-app/src/components/App/index.js
@@ -0,0 +1,80 @@
+// Dependecy Packages
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+// import { QueryClientProvider } from 'react-query';
+import jwt_decode from 'jwt-decode';
+
+// Action dependencies
+import setAuthToken from '../../utilities/setAuthToken';
+import { setActiveUser, logoutUser } from '../../actions/authActions';
+import { setFavorite } from '../../actions/favoriteActions';
+
+// Component dependencies
+import MoviesList from '../MovieList';
+import MovieCard from '../MovieCard';
+// import TVList from '../TVList';
+// import TVCard from '../TVCard';
+import NavBar from '../NavBar';
+import Register from '../Register';
+import Login from '../Login';
+
+// Style dependencies
+import { StyledApp, GlobalStyle } from './App.styled';
+
+//State store
+import store from '../../store';
+
+// Check for token
+if (localStorage.jwtToken) {
+	// Set auth token to header auth
+	setAuthToken(localStorage.jwtToken);
+	// Decode token and get user info and exp
+	const decoded = jwt_decode(localStorage.jwtToken);
+
+	// Check for expired token
+	const currentTime = Date.now() / 1000;
+	if (decoded.exp < currentTime) {
+		// Log user out
+		store.dispatch(logoutUser());
+		// Redirect to login
+		window.location.href = '/login';
+	} else {
+		// Set user, isAuthenticated and hearted films
+		store.dispatch(setActiveUser(decoded));
+		store.dispatch(setFavorite());
+	}
+}
+
+
+
+const App = () => (
+    // <QueryClientProvider>
+	<Provider store={store}>
+      <Router>
+          <StyledApp>
+              <GlobalStyle />
+              <NavBar />
+              <Switch>
+                    <Route exact path="/" component={MoviesList} />
+                    {/* <Route exact path="/" component={TVList} /> */}
+                    <Route exact path="/p=:page" component={MoviesList} />
+                    {/* <Route exact path="/p=:page" component={TVList} /> */}
+                    <Route exact path="/search=:query" component={MoviesList} />
+                    {/* <Route exact path="/search=:query" component={TVList} /> */}
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/register" component={Register} />
+                    <Route exact path="/favorite" render={() => <MoviesList showFavorite={true} />} />
+                    {/* <Route exact path="/favorite" render={() => <TVList showFavorite={true} />} /> */}
+                    <Route path="/search=:query/p=:page" component={MoviesList} />
+                    {/* <Route path="/search=:query/p=:page" component={TVList} /> */}
+                    <Route path="/:id" component={MovieCard} />
+                    {/* <Route path="/:id" component={TVCard} /> */}
+              </Switch>
+          </StyledApp>
+      </Router>
+	</Provider>
+    // </QueryClientProvider>
+);
+
+export default App;
