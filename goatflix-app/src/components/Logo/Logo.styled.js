70737d773c88264afdddaea9490db807b698dcba
GDusek <70817559+GDusek@users.noreply.github.com>
2021-02-01 12:27:02 -0600

    Adjusted logo size

---
+++ goatflix-app/src/components/Logo/Logo.styled.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const StyledLogo = styled.div`
+	display: flex;
+	align-items: space-evenly
+`;
+
+export const StyledGoat = styled.img`
+	margin-left: 1.2rem;
+	margin-top: 1rem;
+	max-height: 454px;
+	max-width: 473px;
+	height: 12vh;
+	width: auto;
+`;
+
+
+export const StyledWords = styled.img`
+	
+	max-height: 150px;
+	max-width: 600px;
+	height: auto;
+	width: auto;
+`;
---
+++ goatflix-app/src/components/Logo/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyledLogo, StyledGoat, StyledWords} from './Logo.styled';
+
+const Logo = () => {
+	return (
+		<StyledLogo>
+			<StyledGoat 
+				id="goat" src="https://res.cloudinary.com/gregdusek/image/upload/v1609810374/GOATflix/goat_nnchqt.png" alt=""/>
+			<StyledWords 
+				id="words" src="https://res.cloudinary.com/gregdusek/image/upload/v1612079771/GOATflix/wordlogo_ykvghy.png" alt=""/>
+		</StyledLogo>
+	);
+};
+
+export default Logo;
