bd88f0e51f67608d17aa38fc5b887366d1ba60b0
GDusek <70817559+GDusek@users.noreply.github.com>
2021-02-01 19:33:59 -0600

    Added store and provider import

--- goatflix-app/src/index.js
+++ goatflix-app/src/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import store from './store';
+import {QueryClientProvider, QueryClient} from 'react-query';
 import {BrowserRouter as Router} from 'react-router-dom';
 import App from './components/App';
-import {QueryClientProvider, QueryClient} from 'react-query';
 import * as serviceWorker from './serviceWorker';
 
 require('dotenv').config();
 const queryClient = new QueryClient();
+const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-<QueryClientProvider client={queryClient}>
-        <Router>
+<Provider store={store}>
+    <Router>
+        <QueryClientProvider client={queryClient}>
             <App />
-        </Router>
-    {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-</QueryClientProvider>,
-document.getElementById("root"));
+        </QueryClientProvider>
+    </Router>
+</Provider>,
+rootElement)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
